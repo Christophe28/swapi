d23c6c19b1d1850b6ef78e60932065ba69975d00
christophe <christophebuffe@gmail.com>
2022-04-20 17:22:08 +0200

    cache ok

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Cards = ({ data, setElement }) => {
-    console.log(data);
+const Cards = ({ data, setPeople, setPlanets }) => {
+
   return (
     <div className="container-cards">
       {data.map((element) => (
-        <section key={element.episode_id} onClick={() => console.log(element)}>
+        <section
+            key={element.episode_id} 
+            onClick={ () => {
+                setPeople(element.characters)
+            }}>
           <h3>{element.title}</h3>
           <section className="info">
               <p>Episode : {element.episode_id}</p>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,20 +6,57 @@ import Cards from '../components/cards';
 const Home = () => {
     const [data, setData] = useState([]);
     const [people, setPeople] = useState([]);
+    const [cache, setCache] = useState({});
+
+    console.log("cache");
     useEffect(async () => {
         const result = await axios("https://swapi.dev/api/films");
         setData(result.data.results);
     }, []);
 
+    useEffect(
+        () => {
+            people.forEach(peopleUrl => getPeople(peopleUrl))
+        },
+        [people]
+    )
+
+    const getPeople = async (peopleUrl) => {
+        if (cache[peopleUrl]) {
+            console.log('en cache !')
+            return cache[peopleUrl]
+        } else {
+            console.log('PAS en cache !')
+            const result = await axios(peopleUrl);
+            setCache(cache => {
+                cache[peopleUrl] = result.data
+                return cache
+            })
+            return result.data
+        }
+    }
+
+    const getPlanets = async (planetUrls) => {
+        if (cache[planetUrls]) {
+            return cache[planetUrls]
+        } else {
+            const result = await axios(planetUrls);
+            setCache(cache => {
+                cache[planetUrls] = result.data
+                return cache
+            })
+            return result.data
+        }
+    }
+
     return (
         <div className='container-home'>
-            <h1>Home</h1>
             <Cards
                 data={data}
-                setElement={setPeople}
+                setPeople={setPeople}
             />
             <section className="container-people">
-                {people}
+                {people.map(people => <p>{cache[people] && cache[people].name}</p>)}
             </section>
         </div>
     );
