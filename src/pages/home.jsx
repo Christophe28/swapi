830792f2a30963b7b0d73ab732caaebcc5f3bcba
Tstassin <Tstassin@users.noreply.github.com>
2022-04-21 11:44:55 +0200

    Implement basic url caching system

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,9 +8,41 @@ const Home = () => {
   const [cache, setCache] = useState({});
 
   useEffect(async () => {
-    const response = await axios("https://swapi.dev/api/films");
-    setAllFilms(response.data.results);
+    const data = await getUrl("https://swapi.dev/api/films");
+    setAllFilms(data.results);
   }, []);
+
+  const isCached = (url) => {
+    if (cache[url]) {
+      return true
+    }
+    else return false
+  }
+
+  const getFromCache = (url) => {
+    return cache[url]
+  }
+
+  const setIntoCache = (url, data) => {
+    setCache(
+      oldCache => {
+        const newCache = {...oldCache}
+        newCache[url] = data
+        return newCache
+      }
+    )
+  }
+
+  const getUrl = async (url) => {
+    if (isCached(url)) {
+      return getFromCache(url);
+    } else {
+      const response = await axios(url);
+      const data = response.data
+      setIntoCache(url, data)
+      return data;
+    }
+  };
 
   return (
     <div className="container-home">
